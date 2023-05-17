ae455fddec978a7b8b0b4d5ec3227525c1f9adee
ingenieroSuarez <correo.fernandosanchez@gmail.com>
2023-05-17 00:14:01 -0500

    firma con token

--- app/middleware/validarAutorizacion.js
+++ app/middleware/validarAutorizacion.js
@@ -1,11 +1,23 @@
+const jwt = require('jsonwebtoken');
 const validarAutorizacion=(req, res, next)=>{
     const token= req.headers.authorization?.split(' ').pop();
     if(!token){
         res.status(409)
         res.send({error: 'sin autorizacion'})
     }else{
-        next()
+        jwt.verify(token, process.env.SECRET_JWT, (error, decoded) => {
+            if (error) {
+              console.error('Error al verificar el token:', error);
+              res.status(409)
+              res.send({error: 'sin autorizacion'})
+            }
+            console.log('Token verificado. Decodificado:', decoded);
+            next()
+          });
     }
+
+
+
 }
 module.exports={ 
     validarAutorizacion
